057024f51058a2621da93ff9faca6b6f4293de56
Luke Curley <kixelated@gmail.com>
2025-09-11 10:35:36 -0700

    Fix URL being used as a key. (#587)

--- js/moq/src/connect.ts
+++ js/moq/src/connect.ts
@@ -27,7 +27,7 @@ export interface ConnectProps {
 }
 
 // Save if WebSocket won the last race, so we won't give QUIC a head start next time.
-const websocketWon = new Map<URL, boolean>();
+const websocketWon = new Set<string>();
 
 /**
  * Establishes a connection to a MOQ server.
@@ -46,7 +46,7 @@ export async function connect(url: URL, props?: ConnectProps): Promise<Connectio
 
 	// Give QUIC a 200ms head start to connect before trying WebSocket, unless WebSocket has won in the past.
 	// NOTE that QUIC should be faster because it involves 1/2 fewer RTTs.
-	const headstart = !webtransport || websocketWon.get(url) ? 0 : (props?.websocket?.delay ?? 200);
+	const headstart = !webtransport || websocketWon.has(url.toString()) ? 0 : (props?.websocket?.delay ?? 200);
 	const websocket =
 		props?.websocket?.enabled !== false
 			? connectWebSocket(props?.websocket?.url ?? url, headstart, cancel)
@@ -67,7 +67,7 @@ export async function connect(url: URL, props?: ConnectProps): Promise<Connectio
 	// Save if WebSocket won the last race, so we won't give QUIC a head start next time.
 	if (quic instanceof WebTransportWs) {
 		console.warn(url.toString(), "using WebSocket fallback; the user experience may be degraded");
-		websocketWon.set(url, true);
+		websocketWon.add(url.toString());
 	}
 
 	// moq-rs currently requires the ROLE extension to be set.
